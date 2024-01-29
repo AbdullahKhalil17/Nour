91a5e32fcfc1886c5fde05a56a49ef4859ce7341
Abdullah A. Khalil <k.goma959@gmail.com>
2024-01-29 07:34:46 +0200

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,72 @@
+        <!--=================================
+        footer -->
+        <footer class="bg-white p-4">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="text-center text-md-start">
+                <p class="mb-0"> &copy; Copyright <span id="copyright"> <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script></span>. <a href="#"> Webmin </a> All Rights Reserved. </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <ul class="text-center text-md-end">
+                <li class="list-inline-item"><a href="#">Terms & Conditions </a> </li>
+                <li class="list-inline-item"><a href="#">API Use Policy </a> </li>
+                <li class="list-inline-item"><a href="#">Privacy Policy </a> </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+        <!--=================================
+        footer -->
+      </div>
+      <!--=================================
+      wrapper -->
+    </div>
+    <!--=================================
+    Main content -->
+  </div>
+
+  <!--=================================
+   jquery -->
+
+  <!-- jquery -->
+  <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+  <!-- plugins-jquery -->
+  <script src="assets/js/plugins-jquery.js"></script>
+
+  <!-- plugin_path -->
+  <script>var plugin_path = 'js/';</script>
+
+  <!-- chart -->
+  <script src="assets/js/chart-init.js"></script>
+
+  <!-- calendar -->
+  <script src="assets/js/calendar.init.js"></script>
+
+  <!-- charts sparkline -->
+  <script src="assets/js/sparkline.init.js"></script>
+
+  <!-- charts morris -->
+  <script src="assets/js/morris.init.js"></script>
+
+  <!-- datepicker -->
+  <script src="assets/js/datepicker.js"></script>
+
+  <!-- sweetalert2 -->
+  <script src="assets/js/sweetalert2.js"></script>
+
+  <!-- toastr -->
+  <script src="assets/js/toastr.js"></script>
+
+  <!-- validation -->
+  <script src="assets/js/validation.js"></script>
+
+  <!-- lobilist -->
+  <script src="assets/js/lobilist.js"></script>
+
+  <!-- custom -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        #myButton {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            text-align: center;
+            text-decoration: none;
+            cursor: pointer;
+            border: 1px solid #3498db;
+            color: #3498db;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        #myButton:hover {
+            background-color: #3498db;
+            color: #fff;
+        }
+    </style>
+    <title>Nour</title>
+</head>
+<body>
+    <a id="myButton" href="admin/index.php">الذهاب الى لوحة التحكم</a>
+</body>
+</html>
