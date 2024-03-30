40890c6c9fca56ac52837c12e5a04540eb0cfacf
Abdullah A. Khalil <k.goma959@gmail.com>
2024-03-30 02:43:57 +0200

    Edit table

--- nour.sql
+++ nour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 28, 2024 at 09:20 PM
+-- Generation Time: Mar 30, 2024 at 12:43 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.3
 
@@ -61,7 +61,61 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `client_name`, `client_pass`, `client_email`, `client_phone`, `client_address`) VALUES
-(4, 'نور', '123456789', 'nour@text.com', '0111115993395', 'الخانكة');
+(4, 'نور', '123456789', 'nour@qqqq.comaaa', '0111115993395', 'الخانكة'),
+(5, 'نور', '123456789', 'nour@text.com', '0111115993395', 'qqqqq'),
+(6, 'نور', '123456789', 'nour@text.com', '0111115993395', 'qqqqq'),
+(7, 'نور', '123456789', 'nour@text.com', '0111115993395', 'qqqqq'),
+(8, 'نور', '123456789', 'nour@text.com', '0111115993395', 'qqqqq');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `client_id` int NOT NULL,
+  `order_total` decimal(10,2) NOT NULL,
+  `order_status` enum('1','2') NOT NULL DEFAULT '2',
+  `order_date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id` int NOT NULL,
+  `order_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  `qty` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int NOT NULL,
+  `product_name` varchar(75) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image_product` varchar(255) NOT NULL,
+  `made_in` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `image_product`, `made_in`) VALUES
+(1, 'Raya Frye', 784.00, '431898301_424457473301714_2370654459212605123_n.jpg', 'Hillary Martinez'),
+(2, 'Pandora Keller', 669.00, '431266564_8236422993041407_616906614829628963_n.jpg', 'Walter Mejia');
 
 --
 -- Indexes for dumped tables
@@ -80,6 +134,27 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`,`product_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,7 +168,42 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
